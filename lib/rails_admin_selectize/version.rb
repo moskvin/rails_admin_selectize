37f187471be5d47b9f1007075c0b8dbd60fcfa6c
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-01-20 15:43:25 +0000

    version bump

--- lib/rails_admin_selectize/version.rb
+++ lib/rails_admin_selectize/version.rb
@@ -1,3 +1,4 @@
 module RailsAdminSelectize
-  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`}"
+  # VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`}"
+  VERSION = "2.0.6"
 end
