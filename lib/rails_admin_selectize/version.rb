098999ed6e86d9fdfe4fd3d8c7996bac7f46b0bc
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2021-01-20 16:46:25 +0100

    Update version.rb

--- lib/rails_admin_selectize/version.rb
+++ lib/rails_admin_selectize/version.rb
@@ -1,4 +1,3 @@
 module RailsAdminSelectize
-  # VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`}"
-  VERSION = "2.0.6"
+  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`}"
 end
